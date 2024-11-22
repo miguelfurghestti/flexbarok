c7fb519e07547755ba36d9746a585010b2d27b4f
Miguel S. Furghestti <miguel.aghei@hotmail.com>
2024-11-22 00:14:30 -0300

    wip

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,50 @@
-<aside class="bg-zinc-900 text-white flex flex-col items-center">
-    navbar
+<aside class="bg-zinc-900 text-white flex flex-col items-center justify-between">
+
+    <div class="w-full">
+        <a href="" class="w-full py-3 bg-[#B1EE81] flex flex-col items-center">
+            <x-jam-home class="w-[38px] h-[50px] text-zinc-800" stroke-width="1" />
+            <span class="text-zinc-800 font-semibold text-sm">INÍCIO</span>
+        </a>
+
+        <a href="" class="w-full py-3 flex flex-col items-center hover:bg-zinc-950 transition duration-500">
+            <x-hugeicons-note-edit class="w-[38px] h-[50px] text-[#B1EE81]" stroke-width="1" />
+            <span class="text-white font-semibold text-xs">COMANDAS</span>
+        </a>
+
+        <a href="" class="w-full py-3 flex flex-col items-center hover:bg-zinc-950 transition duration-500">
+            <x-tabler-soccer-field class="w-[42px] h-[50px] text-[#B1EE81]" stroke-width="1" />
+            <span class="text-white font-semibold text-xs">QUADRAS</span>
+        </a>
+
+        <a href="" class="w-full py-3 flex flex-col items-center hover:bg-zinc-950 transition duration-500">
+            <x-tabler-user-square-rounded class="w-[42px] h-[50px] text-[#B1EE81]" stroke-width="1" />
+            <span class="text-white font-semibold text-xs">CLIENTES</span>
+        </a>
+
+        <a href="" class="w-full py-3 flex flex-col items-center hover:bg-zinc-950 transition duration-500">
+            <x-tabler-calendar-check class="w-[42px] h-[50px] text-[#B1EE81]" stroke-width="1" />
+            <span class="text-white font-semibold text-xs">RESERVAS</span>
+        </a>
+
+        <a href="" class="w-full py-3 flex flex-col items-center hover:bg-zinc-950 transition duration-500">
+            <x-hugeicons-dish-01 class="w-[42px] h-[50px] text-[#B1EE81]" stroke-width="1" />
+            <span class="text-white font-semibold text-xs">CARDÁPIO</span>
+        </a>
+    </div>
+
+    <div class="w-full flex flex-col items-center gap-3 justify-center py-3">
+        <a href="" class="flex flex-row items-center gap-1 group transition duration-300">
+            <x-tabler-settings class="w-5 h-5 text-zinc-500 group-hover:text-white transition duration-300" stroke-width="1" />
+            <span class="text-xs text-zinc-500 group-hover:text-white transition duration-300">
+                AJUSTES
+            </span>
+        </a>
+
+
+        <a href="" class="flex flex-row items-center gap-1 group transition duration-300">
+            <x-tabler-help class="w-5 h-5 text-zinc-500 group-hover:text-white transition duration-300" stroke-width="1" />
+            <span class="text-xs text-zinc-500 group-hover:text-white transition duration-300">AJUDA</span>
+        </a>
+    </div>
+
 </aside>
--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -1,38 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>FlexBar</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
-          theme: {
-            extend: {
-              colors: {
-                pgreen: '#B1EE81',
-              }
+            theme: {
+                fontFamily: {
+                    sans: ['"PT Sans"', 'sans-serif']
+                },
+                extend: {
+                    colors: {
+                        pgreen: '#B1EE81',
+                    }
+                }
             }
-          }
         }
-      </script>
+    </script>
 </head>
+
 <body class="bg-zinc-950">
 
     <div class="grid grid-cols-[110px_1fr_325px] h-screen">
-    @include("components.navbar")
+        @include("components.navbar")
 
-    @yield('conteudo')
+        <div>
+            <div class="grid grid-cols-[200px_1fr] py-9 bg-zinc-900">
+                <section class="bg-zinc-900 flex justify-center">
+                    <img src="img/logo.png" alt="Logo FlexBar">
+                </section>
 
-    <!-- Barra lateral direita -->
-    <aside class="bg-zinc-900">
-    <div class="p-4">
-        <h2 class="text-lg font-semibold">Informações</h2>
-        <p>Este é o painel da direita.</p>
+                <div>
+                    Pesquisa
+                </div>
+            </div>
+
+            <main>
+                @yield('conteudo')
+            </main>
+        </div>
+
+        <!-- Barra lateral direita -->
+        <aside class="bg-zinc-900">
+            <div class="p-4">
+                <h2 class="text-lg font-semibold">Informações</h2>
+                <p>Este é o painel da direita.</p>
+            </div>
+        </aside>
+
     </div>
-</aside>
-
-</div>
 </body>
+
 </html>
