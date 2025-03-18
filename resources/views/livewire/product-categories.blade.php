c2cdd9e7ac00291ffcad119bcd88b5dd0050cb09
miguelsfurghestti <miguelsfurghestti@icloud.com>
2025-03-17 21:16:46 -0300

    wip

--- resources/views/livewire/product-categories.blade.php
+++ resources/views/livewire/product-categories.blade.php
@@ -19,28 +19,44 @@
             </div>
         </div>
         
-        <div class="bg-green-100 text-zinc-900 p-2 rounded-2xl">
-            <span>icone</span>
-            <h1>Bebidas</h1>
-            <p>15 items</p>
+        <div class="flex flex-col justify-start bg-green-100 text-zinc-900 p-3 rounded-2xl gap-8">
+            <x-fas-wine-glass-empty class="w-[30px] h-[30px] text-zinc-700" />
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Bebidas</h1>
+                <p class="text-xs font-medium">15 items</p>
+            </div>
         </div>
 
-        <div class="bg-green-100 text-zinc-900 p-2 rounded-2xl">
-            <span>icone</span>
-            <h1>Bebidas</h1>
-            <p>15 items</p>
+        <div class="flex flex-col justify-start bg-green-100 text-zinc-900 p-3 rounded-2xl gap-8">
+            <x-fas-wine-glass-empty class="w-[30px] h-[30px] text-zinc-700" />
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Bebidas</h1>
+                <p class="text-xs font-medium">15 items</p>
+            </div>
         </div>
 
-        <div class="bg-green-100 text-zinc-900 p-2 rounded-2xl">
-            <span>icone</span>
-            <h1>Bebidas</h1>
-            <p>15 items</p>
+        <div class="flex flex-col justify-start bg-green-100 text-zinc-900 p-3 rounded-2xl gap-8">
+            <x-fas-wine-glass-empty class="w-[30px] h-[30px] text-zinc-700" />
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Bebidas</h1>
+                <p class="text-xs font-medium">15 items</p>
+            </div>
         </div>
 
-        <div class="bg-green-100 text-zinc-900 p-2 rounded-2xl">
-            <span>icone</span>
-            <h1>Bebidas</h1>
-            <p>15 items</p>
+        <div class="flex flex-col justify-start bg-green-100 text-zinc-900 p-3 rounded-2xl gap-8">
+            <x-fas-wine-glass-empty class="w-[30px] h-[30px] text-zinc-700" />
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Bebidas</h1>
+                <p class="text-xs font-medium">15 items</p>
+            </div>
+        </div>
+
+        <div class="flex flex-col justify-start bg-green-100 text-zinc-900 p-3 rounded-2xl gap-8">
+            <x-fas-wine-glass-empty class="w-[30px] h-[30px] text-zinc-700" />
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Bebidas</h1>
+                <p class="text-xs font-medium">15 items</p>
+            </div>
         </div>
 
     </div>  
