9abf9a76b6c7e3f6e000b61f5ceae4c930d62753
miguelsfurghestti <miguelsfurghestti@icloud.com>
2025-03-21 16:53:13 -0300

    wip

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -6,6 +6,26 @@ use Livewire\Component;
 
 class Products extends Component
 {
+    public $name;
+    public $description;
+    public $price;
+    public $qty;
+
+    public $showModal = false;
+    public $showEditModal = false;
+    public $showDeleteModal = false;
+
+    public $productIdToEdit;
+    public $productIdToDelete;
+
+    protected function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'price' => 'required',
+        ];
+    }
+
     public function render()
     {
         return view('livewire.products');
